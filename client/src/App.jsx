9af1e374b5a49f680d93e6d9ca415b418698420a
TOM SABU <tomsabu0000@gmail.com>
2024-10-02 12:46:42 +0530

    Refactor Chatbot component to use EmotionDetection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/chatbot" element={<EmotionDetection />} />
         <Route path="/emotion-detection" element={<EmotionDetection />} />
       </Routes>
       {/* <Footer /> */}
--- client/src/components/Chatbot.jsx
+++ client/src/components/Chatbot.jsx
@@ -1,43 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import AccpectTerms from "./Accpect_terms"; 
+import React from 'react'
 
-const Chatbot = () => {
-  const navigate = useNavigate();
-  const [isConsentGiven, setIsConsentGiven] = useState(false);
-
-  useEffect(() => {
-    const consent = localStorage.getItem("hasVisited");
-    if (!consent) {
-      setIsConsentGiven(false); 
-    } else {
-      setIsConsentGiven(true); 
-    }
-  }, []);
-
-  const handleConsent = () => {
-    setIsConsentGiven(true);
-    localStorage.setItem("hasVisited", "true");
-  };
-
-  const handleDeny = () => {
-
-    localStorage.removeItem("hasVisited"); 
-    navigate("/");
-  };
-
+function Chatbot() {
   return (
-    <div>
-      {!isConsentGiven ? (
-        <AccpectTerms onConsent={handleConsent} onDeny={handleDeny} />
-      ) : (
-        <div>
-          <h1>Emotion Detection Chatbot</h1>
-          <p>This is your chatbot that analyzes emotions using AI!</p>
-        </div>
-      )}
-    </div>
-  );
-};
+    <div>Chatbot</div>
+  )
+}
 
-export default Chatbot;
+export default Chatbot
